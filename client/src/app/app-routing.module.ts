059cadad3dad3f1f6fdc04d37d4c597a08df3e9e
ismailERMİŞ <ismailermis@gmail.com>
2024-10-25 20:26:45 +0300

    login and register component added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},// layzloading
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
